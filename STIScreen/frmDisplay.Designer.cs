be0ef630f9f4a5a3f2b2393b11ef8ccd678353af
Giowmi <Giowmi@GIOWMI-PC>
2018-05-10 16:14:33 +0800

    Add project files.

---
+++ STIScreen/frmDisplay.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace STIScreen
+{
+    partial class frmDisplay
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmDisplay));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblCash = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lblAcc = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.lblReg = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblClock = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblNews = new System.Windows.Forms.Label();
+            this.timerNews = new System.Windows.Forms.Timer(this.components);
+            this.timerClock = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.lblCash);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Bernard MT Condensed", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(440, 540);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Window 3";
+            // 
+            // lblCash
+            // 
+            this.lblCash.Font = new System.Drawing.Font("Arial Rounded MT Bold", 140.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCash.Location = new System.Drawing.Point(0, 143);
+            this.lblCash.Name = "lblCash";
+            this.lblCash.Size = new System.Drawing.Size(440, 394);
+            this.lblCash.TabIndex = 13;
+            this.lblCash.Text = "888";
+            this.lblCash.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblCash.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Lucida Bright", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkRed;
+            this.label1.Location = new System.Drawing.Point(0, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(440, 96);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Cashier";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.groupBox2.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox2.Controls.Add(this.lblAcc);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Font = new System.Drawing.Font("Bernard MT Condensed", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(463, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(440, 540);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Window 2";
+            // 
+            // lblAcc
+            // 
+            this.lblAcc.Font = new System.Drawing.Font("Arial Rounded MT Bold", 140.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAcc.Location = new System.Drawing.Point(6, 143);
+            this.lblAcc.Name = "lblAcc";
+            this.lblAcc.Size = new System.Drawing.Size(428, 394);
+            this.lblAcc.TabIndex = 14;
+            this.lblAcc.Text = "888";
+            this.lblAcc.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblAcc.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Lucida Bright", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DarkRed;
+            this.label2.Location = new System.Drawing.Point(0, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(434, 96);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Registrar";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.groupBox3.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox3.Controls.Add(this.lblReg);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Font = new System.Drawing.Font("Bernard MT Condensed", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox3.Location = new System.Drawing.Point(914, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(440, 540);
+            this.groupBox3.TabIndex = 1;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Window 1";
+            // 
+            // lblReg
+            // 
+            this.lblReg.Font = new System.Drawing.Font("Arial Rounded MT Bold", 140.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblReg.Location = new System.Drawing.Point(0, 143);
+            this.lblReg.Name = "lblReg";
+            this.lblReg.Size = new System.Drawing.Size(434, 394);
+            this.lblReg.TabIndex = 15;
+            this.lblReg.Text = "888";
+            this.lblReg.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblReg.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Lucida Bright", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.DarkRed;
+            this.label3.Location = new System.Drawing.Point(0, 47);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(434, 96);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "Accounting";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblClock
+            // 
+            this.lblClock.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblClock.BackColor = System.Drawing.Color.Transparent;
+            this.lblClock.Font = new System.Drawing.Font("Elephant", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblClock.ForeColor = System.Drawing.Color.Black;
+            this.lblClock.Location = new System.Drawing.Point(12, 654);
+            this.lblClock.Name = "lblClock";
+            this.lblClock.Size = new System.Drawing.Size(1342, 88);
+            this.lblClock.TabIndex = 2;
+            this.lblClock.Text = "00:00";
+            this.lblClock.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox1.BackColor = System.Drawing.Color.LightCyan;
+            this.pictureBox1.Location = new System.Drawing.Point(-2, 558);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1373, 69);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblNews
+            // 
+            this.lblNews.AutoSize = true;
+            this.lblNews.BackColor = System.Drawing.Color.LightCyan;
+            this.lblNews.Font = new System.Drawing.Font("Lucida Fax", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNews.Location = new System.Drawing.Point(1360, 569);
+            this.lblNews.Name = "lblNews";
+            this.lblNews.Size = new System.Drawing.Size(1110, 43);
+            this.lblNews.TabIndex = 4;
+            this.lblNews.Text = "PLEASE WAIT FOR YOUR NUMBER TO FLASH ON SCREEN";
+            // 
+            // timerNews
+            // 
+            this.timerNews.Enabled = true;
+            this.timerNews.Interval = 30;
+            this.timerNews.Tick += new System.EventHandler(this.timerNews_Tick);
+            // 
+            // timerClock
+            // 
+            this.timerClock.Enabled = true;
+            this.timerClock.Tick += new System.EventHandler(this.timerClock_Tick);
+            // 
+            // frmDisplay
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Yellow;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1366, 768);
+            this.Controls.Add(this.lblClock);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lblNews);
+            this.Controls.Add(this.pictureBox1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmDisplay";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "STI QUEUEING SYSTEM";
+            this.TopMost = true;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label lblClock;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblNews;
+        private System.Windows.Forms.Timer timerNews;
+        private System.Windows.Forms.Timer timerClock;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblCash;
+        private System.Windows.Forms.Label lblAcc;
+        private System.Windows.Forms.Label lblReg;
+    }
+}
+
---
+++ STIScreen/frmDisplay.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.Collections;
+
+namespace STIScreen
+{
+    public partial class frmDisplay : Form
+    {
+        MySqlConnection con = new MySqlConnection("datasource=localhost;port=3306;username=root;password='1234'");
+        /// <summary>
+        /// PUBLIC VARIABLES:
+        int WindowNo, count;
+        string TicketNo;
+        /// </summary>
+        public frmDisplay()
+        {
+            InitializeComponent();
+            getFromDb();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //lblCashier1.Visible = true;
+            //lblCashier1.Text = "0001";
+        }
+        private void timerClock_Tick(object sender, EventArgs e)
+        {
+            lblClock.Text = DateTime.Now.ToString("MMM dd, yyyy hh:mm:ss tt");
+        }
+        void getFromDb()
+        {
+            MySqlCommand cmd = new MySqlCommand("Select * from dbstiqueue.tblscreen where Served = 0 limit 10 ", con);
+            MySqlDataReader reader;
+            con.Open();
+
+            reader = cmd.ExecuteReader();
+            count = 0;
+            while (reader.Read())
+            {
+                count = count + 1;
+                TicketNo = reader.GetString(0);
+                WindowNo = reader.GetInt32(1);
+                if (WindowNo == 1)
+                {
+                    Window1();
+                }
+                else if (WindowNo == 2)
+                {
+                    Window2();
+                }
+                else if (WindowNo == 3)
+                {
+                    Window3();
+                }
+            }
+            con.Close();
+        }
+        void Window1()
+        {
+            lblReg.Visible = true;
+            lblReg.Text = TicketNo;
+        }
+        void Window2()
+        {
+            lblReg.Visible = true;
+            lblAcc.Text = TicketNo;
+        }
+        void Window3()
+        {
+            lblCash.Visible = true;
+            lblCash.Text = TicketNo;
+        }
+
+
+        private void timerNews_Tick(object sender, EventArgs e)
+        {
+            if (lblNews.Left == (0 - lblNews.Width))
+            {
+                lblNews.Location = new Point(1360, 569);
+            }
+            else
+            {
+                lblNews.Left = lblNews.Left - 5;
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
